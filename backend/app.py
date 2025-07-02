3187d6a7f2bf3747cbe44e5a3f7355fe9e383411
nandyiitm <22f3002857@ds.study.iitm.ac.in>
2025-07-02 16:57:43 +0530

    Added html for fetching api info

--- backend/app.py
+++ backend/app.py
@@ -26,6 +26,16 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()
 
+        # users = {
+        #     User(email='user0@mad2.com', name='User0', password='12345678'),
+        #     User(email='user1@mad2.com', name='User1', password='12345678'),
+        #     User(email='user2@mad2.com', name='User2', password='12345678'),
+        #     User(email='user3@mad2.com', name='User3', password='12345678')
+        # }
+        # for user in users:
+        #     db.session.add(user)
+        # db.session.commit()
+
         admin = User.query.filter_by(email='admin@mad2.com').first()
         if not admin:
             user = User(email='admin@mad2.com', name='Admin', password='12345678', role='admin')
